589a73b904a8b9d6e6ec45dcf5d5a1a0e68c0a88
Darshan <er.darshanghetiya@gmail.com>
2023-10-09 20:11:34 +0530

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <h1 className="App">Hello From BnD!</h1>;
+  return (
+      <h1>
+        hello
+        <br />
+        {process.env.REACT_APP_NAME}
+
+        <br />
+
+        Address :   {process.env.REACT_APP_ADDRESS}
+
+        <br />
+        Country :   {process.env.REACT_APP_COUNTRY}
+
+      </h1>
+    );
 }
 
 export default App;
